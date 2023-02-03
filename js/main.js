5556c55636a95561beb6d00b1578147171c20077
DaDoDev7 <dario.plastina@gmail.com>
2023-02-03 15:01:30 +0100

    evaluation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
-$(document).ready(function(){
-    $(".hamburger").click(function(){
-      $(this).toggleClass("active");
+//arrowup
+
+const arrowUp = document.querySelector(".arrowfixedright");
+arrowUp.addEventListener("click", function() {
+    window.scroll({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
     });
-  });
+});
+
+//burgermenu
+
+const burgerMenu = document.querySelector(".burgermenu");
+    burgerMenu.addEventListener("click", function() {
+        burgerMenu.classList.toggle("change");
+    });
